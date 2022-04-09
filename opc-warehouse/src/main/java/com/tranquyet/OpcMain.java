440d42ad3f216d75f0dfff0c4d6868ca3e26fa8e
tranquyet <quyettran057@gmail.com>
2022-04-09 13:17:52 +0700

    initialize project

---
+++ opc-warehouse/src/main/java/com/tranquyet/OpcMain.java
@@ -0,0 +1,15 @@
+package com.tranquyet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpcMain {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(OpcMain.class, args);
+		System.out.println("OPC MAIN");
+	}
+
+}
---
+++ supervise-warehouse/src/main/java/com/tranquyet/SuperviseMain.java
@@ -0,0 +1,14 @@
+package com.tranquyet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperviseMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperviseMain.class, args);
+		System.out.println("SUPERVISE MAIN");
+	}
+
+}
